78c3d5f1649a8603b99ea801cddbb9e433924d40
PassosLucasB <PassosL@outlook.com>
2024-11-23 16:41:31 -0300

    Inicio do desafio

--- JavaNinja/src/Desafio_1/Desafio.java
+++ JavaNinja/src/Desafio_1/Desafio.java
@@ -3,5 +3,31 @@ package Desafio_1;
 public class Desafio {
     public static void main(String[] args) {
 
+        // Ninja 1
+        String nomeNinja1 = "Joao";
+        int idade = 20;
+        String missao = "Pagar o aluguel";
+        String statusDaMissao = "Pago";
+        char nivelDaMissao = 'A';
+
+
+        // Ninja 2
+        String nomeNinja2 = "Pedro";
+        int idade = 21;
+        String missao = "Pagar a luz";
+        String statusDaMissao = "Pago";
+        char nivelDaMissao = 'A';
+
+
+
+        // Ninja 3
+        String nomeNinja3 = "Mario";
+        int idade = 22;
+        String missao = "Pagar a água";
+        String statusDaMissao = "Pago";
+        char nivelDaMissao = 'A';
+
+
+
     }
 }
