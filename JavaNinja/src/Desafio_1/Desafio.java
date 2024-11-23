885cc42462ba0a98a93ec74be2bb7fb55500cd00
PassosLucasB <PassosL@outlook.com>
2024-11-23 16:49:06 -0300

    Inicio do desafio

--- JavaNinja/src/Desafio_1/Desafio.java
+++ JavaNinja/src/Desafio_1/Desafio.java
@@ -5,29 +5,43 @@ public class Desafio {
 
         // Ninja 1
         String nomeNinja1 = "Joao";
-        int idade = 20;
-        String missao = "Pagar o aluguel";
-        String statusDaMissao = "Pago";
-        char nivelDaMissao = 'A';
+        int idade1 = 20;
+        String missao1 = "Pagar o aluguel";
+        String statusDaMissao1 = "Pago";
+        char nivelDaMissao1 = 'A';
+
+        System.out.println("Nome do ninja 1: " + nomeNinja1);
+        System.out.println("Idade do ninja 1: " + idade1);
+        System.out.println("Missão do ninja 1: " + missao1);
+        System.out.println("Status da missão do ninja 1: " + statusDaMissao1);
+        System.out.println("Nível da missão do ninja 1: " + nivelDaMissao1);
 
 
         // Ninja 2
         String nomeNinja2 = "Pedro";
-        int idade = 21;
-        String missao = "Pagar a luz";
-        String statusDaMissao = "Pago";
-        char nivelDaMissao = 'A';
+        int idade2 = 21;
+        String missao2 = "Pagar a luz";
+        String statusDaMissao2 = "Pago";
+        char nivelDaMissao2 = 'A';
 
-
+        System.out.println("Nome do ninja 2: " + nomeNinja2);
+        System.out.println("Idade do ninja 2: " + idade2);
+        System.out.println("Missão do ninja 2: " + missao2);
+        System.out.println("Status da missão do ninja 2: " + statusDaMissao2);
+        System.out.println("Nível da missão do ninja 2: " + nivelDaMissao2);
 
         // Ninja 3
         String nomeNinja3 = "Mario";
-        int idade = 22;
-        String missao = "Pagar a água";
-        String statusDaMissao = "Pago";
-        char nivelDaMissao = 'A';
+        int idade3 = 22;
+        String missao3 = "Pagar a água";
+        String statusDaMissao3 = "Pago";
+        char nivelDaMissao3 = 'A';
 
-
+        System.out.println("Nome do ninja 3: " + nomeNinja3);
+        System.out.println("Idade do ninja 3: " + idade3);
+        System.out.println("Missão do ninja 3: " + missao3);
+        System.out.println("Status da missão do ninja 3: " + statusDaMissao3);
+        System.out.println("Nível da missão do ninja 3: " + nivelDaMissao3);
 
     }
 }
