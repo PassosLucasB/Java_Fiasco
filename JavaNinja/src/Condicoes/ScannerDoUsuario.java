21efc0519f65de556db902f71a725b711ef4dd11
PassosLucasB <PassosL@outlook.com>
2024-11-23 22:16:23 -0300

    "work in Scanner"

--- JavaNinja/src/Condicoes/ScannerDoUsuario.java
+++ JavaNinja/src/Condicoes/ScannerDoUsuario.java
@@ -1,4 +1,19 @@
 package Condicoes;
 
+import java.nio.Buffer;
+import java.util.Scanner;
+
 public class ScannerDoUsuario {
+    public static void main(String[] args) {
+
+        /*
+         *   Scanner = é um jeito de trazer o usuario para dentro da aplicacao.
+         *   Objetivo: O usuario vai criar um ninja e vamos validar os dados.
+         *
+         */
+
+        Scanner caixaDeTexto = new Scanner(System.in);
+
+        caixaDeTexto.close();
+    }
 }
