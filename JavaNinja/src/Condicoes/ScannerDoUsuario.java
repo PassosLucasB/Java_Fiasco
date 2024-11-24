32b03efc453afda4c9aa47ed247e4783903d05d6
PassosLucasB <PassosL@outlook.com>
2024-11-23 22:28:20 -0300

    "end Scanner"

--- JavaNinja/src/Condicoes/ScannerDoUsuario.java
+++ JavaNinja/src/Condicoes/ScannerDoUsuario.java
@@ -1,6 +1,6 @@
 package Condicoes;
 
-import java.nio.Buffer;
+
 import java.util.Scanner;
 
 public class ScannerDoUsuario {
@@ -12,8 +12,27 @@ public class ScannerDoUsuario {
          *
          */
 
+        // Abrir o Scanner
         Scanner caixaDeTexto = new Scanner(System.in);
 
+        // Receber o nome do ninja
+        System.out.println("Escreva aqui o nome do ninja");
+        String nomeDoNinja = caixaDeTexto.nextLine();
+        System.out.println("O nome do seu ninja é: " + nomeDoNinja);
+
+        // Receber a idade do ninja
+        System.out.println("Escreva aqui a idade do seu ninja: ");
+        int idadeDoNinja = caixaDeTexto.nextInt();
+        System.out.println("A idade do seu ninja é: " + idadeDoNinja + " anos.");
+
+        // Tratamento de dados
+        if(idadeDoNinja >= 18){
+            System.out.println("Esse ninja já é maior de idade e pode ir para missoes fora da aldeia");
+        } else {
+            System.out.println("Esse ninja é muito novo ainda, precisa treinar mais antes de sair da vila");
+        }
+
+        // Fechar sempre o Scanner
         caixaDeTexto.close();
     }
 }
