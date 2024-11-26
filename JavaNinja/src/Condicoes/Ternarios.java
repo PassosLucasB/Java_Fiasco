7111d3b4c8278ec18e9e82f516ae6a8c32f7257d
PassosLucasB <PassosL@outlook.com>
2024-11-26 02:24:02 -0300

    "add Ternarios"

---
+++ JavaNinja/src/Condicoes/Ternarios.java
@@ -0,0 +1,16 @@
+package Condicoes;
+
+public class Ternarios {
+    public static void main(String[] args) {
+
+        /*
+        *   Ternarios: São maneiras de reduzir o codigo
+        *   variavel = (condicao) ? valorSeVerdadeiro : valorSeFalso;
+         */
+
+        short numeroDeMissoes = 1;
+        String nivelDoNinja = (numeroDeMissoes >= 10) ? "Esse ninja esta com mais de 10 missoes" : "Esse ninja tem menos de 10 missoes";
+
+        System.out.println(nivelDoNinja);
+    }
+}
