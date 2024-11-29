d14dc88ffbf5f9d1d0dcb460178d73f1f3c02ad1
PassosLucasB <PassosL@outlook.com>
2024-11-29 00:43:41 -0300

    "end LacosDeRepeticao"

--- JavaNinja/src/Condicoes/LacosDeRepeticao.java
+++ JavaNinja/src/Condicoes/LacosDeRepeticao.java
@@ -3,6 +3,29 @@ package Condicoes;
 public class LacosDeRepeticao {
     public static void main(String[] args) {
 
+        /*
+        *   Laços de Repetição: Vão repetir infinitamente ou até você atingir o paremetro desejado
+        *   WHILE = FOR
+        *
+         */
+
+        //  While
+        // while (condição) {Tudo aque vai acontecer}
+
+        int numeroDeClones = 0;
+        int numeroMaximoDeClones = 40;
+
+        /*while (numeroDeClones <= numeroMaximoDeClones) {
+            numeroDeClones++;
+            System.out.println("O Naruto fez um clone das sombras " + numeroDeClones);
+        }
+        */
+        //  FOR
+
+        for (int i = 0; i <= numeroMaximoDeClones; i++) {
+            System.out.println("O Naruto esta se clonando e ja se clonou " + i);
+        }
+
     }
 
 }
--- JavaNinja/src/TiposDeDados/DadosPrimitivos.java
+++ JavaNinja/src/TiposDeDados/DadosPrimitivos.java
@@ -3,8 +3,16 @@ package TiposDeDados;
 public class DadosPrimitivos {
     public static void main(String[] args) {
 
-        int resultado = (5 > 3) ? 10 : 20;?
+      /*
+      * Dados primitivos - int, double, float, char, boolean, short.
+      * Objetivo da aula: Criar um ninja - Naruto
+      *
+       */
 
-        
+        int idade = 16; // Valor maximo: 2 147 483 647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 999999L; // Valor maximo: 9 223 372 036 854 755 807
     }
 }
