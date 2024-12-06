7636b95ddbf7c96d986e96efaa966c7697a1a321
PassosLucasB <PassosL@outlook.com>
2024-12-06 01:39:59 -0300

    "End Arrays"

--- JavaNinja/src/NivelBasico/Array.java
+++ JavaNinja/src/NivelBasico/Array.java
@@ -3,13 +3,21 @@ package NivelBasico;
 public class Array {
     public static void main(String[] args) {
 
+      String[] ninja = new String [4];
+        ninja[0] = "Naruto Uzumaki";
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+        ninja[3] = "Hinata Hyuga";
+
+        System.out.println(ninja[3]);
+
+     /*
         String ninja1 = "Naruto Uzumaki";
         String ninja2 = "Sasuke Uchiha";
         String ninja3 = "Sakura Haruno";
-        String ninja4 = "Hinata Hyuga";
         System.out.println(ninja1);
         System.out.println(ninja2);
         System.out.println(ninja3);
-        System.out.println(ninja4);
+      */
     }
 }
