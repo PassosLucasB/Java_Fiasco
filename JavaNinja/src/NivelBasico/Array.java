a9853532c2a8d044c7a8bc9742120d2068fe8d79
PassosLucasB <PassosL@outlook.com>
2024-12-06 01:28:50 -0300

    "Arrays"

--- JavaNinja/src/NivelBasico/Array.java
+++ JavaNinja/src/NivelBasico/Array.java
@@ -4,6 +4,12 @@ public class Array {
     public static void main(String[] args) {
 
         String ninja1 = "Naruto Uzumaki";
-        String ninja2 = "Sasuke"
+        String ninja2 = "Sasuke Uchiha";
+        String ninja3 = "Sakura Haruno";
+        String ninja4 = "Hinata Hyuga";
+        System.out.println(ninja1);
+        System.out.println(ninja2);
+        System.out.println(ninja3);
+        System.out.println(ninja4);
     }
 }
