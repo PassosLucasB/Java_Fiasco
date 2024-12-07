d640fa62bcd1bad18b2649f8d66676f93b0eb5d9
PassosLucasB <PassosL@outlook.com>
2024-12-07 01:33:36 -0300

    "Inicialização de Arrays"

--- JavaNinja/src/NivelBasico/Array.java
+++ JavaNinja/src/NivelBasico/Array.java
@@ -3,14 +3,37 @@ package NivelBasico;
 public class Array {
     public static void main(String[] args) {
 
-      String[] ninja = new String [5];
+        // Arrays são tipo referência!
+        // String inicializa com null
+      String[] ninja = new String [6];
         ninja[0] = "Naruto Uzumaki";
         ninja[1] = "Sasuke Uchiha";
         ninja[2] = "Sakura Haruno";
         ninja[3] = "Hinata Hyuga";
         ninja[4] = "Kakashi Hatake";
+        System.out.println(ninja[5]);
 
-        System.out.println(ninja[3]);
+        // Array idade
+        // int inicializa com 0
+        int [] idade = new int[2];
+        System.out.println(idade[1]);
+
+        //  Array boolean
+        // boolean inicializa com false
+        boolean [] verdadeiroOuFalso = new boolean[1];
+        System.out.println(verdadeiroOuFalso[0]);
+
+        // Array doubles
+        // double inicializa com 0.0
+        double[] flutuante = new double[1];
+        System.out.println(flutuante[0]);
+
+
+
+        String nomeDoNinja1 = "Gaara do deserto";
+        String nomeDoNinja2 = "Rock Lee";
+
+
 
      /*
         String ninja1 = "Naruto Uzumaki";
