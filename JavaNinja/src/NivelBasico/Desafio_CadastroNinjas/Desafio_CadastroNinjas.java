85c9b0008a6a3ad325081cee5446ae138e5ebbfc
PassosLucasB <PassosL@outlook.com>
2024-12-24 00:29:58 -0300

    "Início Desafio_CadastroNinjas"

--- JavaNinja/src/NivelBasico/Desafio_CadastroNinjas/Desafio_CadastroNinjas.java
+++ JavaNinja/src/NivelBasico/Desafio_CadastroNinjas/Desafio_CadastroNinjas.java
@@ -1,4 +1,15 @@
 package NivelBasico.Desafio_CadastroNinjas;
 
+import java.sql.SQLOutput;
+
 public class Desafio_CadastroNinjas {
+    public static void main(String[] args) {
+
+        System.out.println("\n===== Menu de Ninjas =====");
+        System.out.println("1. Cadastrar um Ninja");
+        System.out.println("2. Listar Ninjas Cadastrados");
+        System.out.println("3. Sair");
+        System.out.println("Escolha uma opção para prosseguir: ");
+
+    }
 }
