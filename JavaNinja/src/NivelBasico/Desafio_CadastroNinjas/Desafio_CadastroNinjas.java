9594a617c2154a5fb7980681fefc6c6b6d481181
PassosLucasB <PassosL@outlook.com>
2024-12-25 23:30:14 -0300

    "Adicionando o SwitchCase"

--- JavaNinja/src/NivelBasico/Desafio_CadastroNinjas/Desafio_CadastroNinjas.java
+++ JavaNinja/src/NivelBasico/Desafio_CadastroNinjas/Desafio_CadastroNinjas.java
@@ -1,6 +1,8 @@
 package NivelBasico.Desafio_CadastroNinjas;
 
-import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
 
 public class Desafio_CadastroNinjas {
     public static void main(String[] args) {
@@ -28,12 +30,41 @@ public class Desafio_CadastroNinjas {
         * String status de conclusão
         * */
 
+        // Abriu Scanner
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\n===== Menu de Ninjas =====");
+        System.out.println("===== Menu de Ninjas =====");
         System.out.println("1. Cadastrar um Ninja");
         System.out.println("2. Listar Ninjas Cadastrados");
         System.out.println("3. Sair");
         System.out.println("Escolha uma opção para prosseguir: ");
 
+        int opUsuario = scanner.nextInt();
+
+        switch (opUsuario) {
+            case 1:
+                System.out.println("Digite o nome do Ninja a ser cadastrado: ");
+                break;
+            case 2:
+                System.out.println("LISTAR NINJAS - ADICIONAR LISTA AQUI");
+                break;
+            case 3:
+                System.out.println("Você escolheu Sair e Encerrar");
+                break;
+            default:
+                System.out.println("Você não escolheu uma alternativa válida! Tente novamente.");
+        }
+
+
+
+
+
+
+
+
+
+        // Fechar o Scanner
+        scanner.close();
     }
 }
+
