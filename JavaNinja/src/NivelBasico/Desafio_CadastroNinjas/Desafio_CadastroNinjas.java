321f657cc745f260f8585a956ab31cb52f7074d4
PassosLucasB <PassosL@outlook.com>
2024-12-25 22:22:46 -0300

    "Segundo Passo Desafio_CadastroNinjas"

--- JavaNinja/src/NivelBasico/Desafio_CadastroNinjas/Desafio_CadastroNinjas.java
+++ JavaNinja/src/NivelBasico/Desafio_CadastroNinjas/Desafio_CadastroNinjas.java
@@ -5,6 +5,30 @@ import java.sql.SQLOutput;
 public class Desafio_CadastroNinjas {
     public static void main(String[] args) {
 
+        /*
+        * Crie um programa que represente três ninjas da Vila da Folha (Konoha) de "Naruto"
+        * e suas respectivas missões.
+        *
+        * Cada ninja tem um nome, uma idade e uma missão atribuída a ele,
+        * com nome da missão, nível de dificuldade e status de conclusão.
+        *
+        * */
+
+        /*
+        * 3 ninjas
+        *
+        * Objeto - Ninja
+        * String nome
+        * int idade
+        * String missão
+        *
+        * Objeto - Missão
+        * String nome
+        * String nível de dificuldade
+        * String status de conclusão
+        * */
+
+
         System.out.println("\n===== Menu de Ninjas =====");
         System.out.println("1. Cadastrar um Ninja");
         System.out.println("2. Listar Ninjas Cadastrados");
