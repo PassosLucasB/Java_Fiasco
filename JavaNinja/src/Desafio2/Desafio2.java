1db69c368a67eceb9bf4fa22976202039cc50893
PassosLucasB <PassosL@outlook.com>
2025-01-07 01:24:04 -0300

    "Add case1"

--- JavaNinja/src/Desafio2/Desafio2.java
+++ JavaNinja/src/Desafio2/Desafio2.java
@@ -9,13 +9,35 @@ public class Desafio2 {
         Scanner scanner = new Scanner(System.in);
 
         // Fazer o array
+        int NUMERO_MAX = 5;
+        String[] ninjas = new String[NUMERO_MAX];
 
+        // Contadores
+        int ninjasCadastrados = 0;
+        int opcao = 0;
 
-        // Cadastrar os ninjas em um array e depois mostrar opções com Switch cases
-        System.out.println("\n===== Menu Ninja =====");
-        System.out.println("1. Cadastrar Ninja");
-        System.out.println("2. Listar Ninjas");
-        System.out.println("3. Sair");
-        System.out.println("Escolha uma opção: ");
+        while (opcao != 3){
+
+            // MENU
+            System.out.println("\n===== Menu Ninja =====");
+            System.out.println("1. Cadastrar Ninja");
+            System.out.println("2. Listar Ninjas");
+            System.out.println("3. Sair");
+            System.out.println("Escolha uma opção: ");
+
+            switch (opcao) {
+                case 1:
+                    if (ninjasCadastrados < NUMERO_MAX){
+                        System.out.println("Digite o nome do ninja para cadastro: ");
+                        String nomeNinja = scanner.nextLine();
+                        ninjas[ninjasCadastrados] = nomeNinja;
+                        ninjasCadastrados++;
+                        System.out.println("Ninja cadastrado com sucesso");
+                    } else {
+                        System.out.println("A lista de ninjas está cheia");
+                    }
+                    break;
+            }
+        }
     }
 }
