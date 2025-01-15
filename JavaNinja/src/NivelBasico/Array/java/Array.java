83f67b08a8744a5d03e2f19c46c82cb63c83c2e7
PassosLucasB <PassosL@outlook.com>
2025-01-15 09:49:16 -0300

    "Array 2D"

--- JavaNinja/src/NivelBasico/Array/java/Array.java
+++ JavaNinja/src/NivelBasico/Array/java/Array.java
@@ -1,4 +1,10 @@
 package NivelBasico.Array.java;
 
 public class Array {
+    public static void main(String[] args) {
+
+
+        String[] ninjas = new String[3];
+        System.out.println();
+    }
 }
