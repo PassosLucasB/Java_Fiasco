43720adc900d692ec204f8f614a18eb537d6478b
PassosLucasB <PassosL@outlook.com>
2025-01-26 23:27:48 -0300

    "Inicio Orientacao Objeto"

--- JavaNinja/src/NivelIntermediario/Main.java
+++ JavaNinja/src/NivelIntermediario/Main.java
@@ -3,18 +3,32 @@ package NivelIntermediario;
 public class Main {
     public static void main(String[] args) {
 
-        // Criar o ninja Naruto - Naruto é um OBJETO
+        // Objeto 1
        Ninja Naruto = new Ninja();
        Naruto.nome = "Naruto Uzumaki";
        Naruto.aldeia = "Aldeia da Folha";
        Naruto.idade = 17;
 
 
-        // Criar ninja Sasuke Uchiha - Sasuke é um OBJETO
+        // Objeto 2
         Ninja Sasuke = new Ninja();
         Sasuke.nome = "Sasuke Uchiha";
         Sasuke.aldeia = "Aldeia da Folha";
         Sasuke.idade = 18;
+
+        // Objeto 3
+        Ninja Sakura = new Ninja();
+        Sakura.nome = "Sakura Haruno";
+        Sakura.aldeia = "Aldeia da Folha";
+        Sakura.idade = 18;
+
+        // Objeto 4
+        Ninja Hinata = new Ninja();
+        Hinata.nome = "Hinata Hyuga";
+        Hinata.aldeia = "Aldeia da Folha";
+        Hinata.idade = 16;
+
+
 
         // Aplicando métodos no meu OBJETO
         Sasuke.SharinganAtivado();
@@ -24,13 +38,6 @@ public class Main {
         int quantoTempoFalta = Sasuke.anosParaSeTornarHokage(70);
         System.out.println("Você tem: " + Sasuke.idade + " anos então falta no mínimo: " + quantoTempoFalta + " anos.");
 
-
-
-        // Criar Sakura Haruno
-        Ninja Sakura = new Ninja();
-        Sakura.nome = "Sakura Haruno";
-        Sakura.aldeia = "Aldeia da Folha";
-        Sakura.idade = 18;
 
 
     }
