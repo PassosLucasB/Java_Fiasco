69f8241486081037e3c0ce1894f635eb7c86bbd5
PassosLucasB <PassosL@outlook.com>
2025-02-14 23:28:42 -0300

    "Finaliza Construtores "

--- JavaNinja/src/NivelIntermediario/Construtores/Hokages.java
+++ JavaNinja/src/NivelIntermediario/Construtores/Hokages.java
@@ -5,5 +5,43 @@ public class Hokages {
     String nome;
     int idade;
     boolean vivoOuNao;
+    String aldeia;
+    int missoes;
+    double saldoBancario;
+    double altura;
 
+    // No args Constructor =  Construtor sem argumentos
+    public Hokages() {
+    }
+
+    // Criar um construtor com argumentos
+    public Hokages(String nome){
+        this.nome =  nome;
+    }
+
+    public Hokages(int idade){
+        this.idade = idade;
+    }
+
+    // All args constructor = Construtor com todos os argumentos
+    public Hokages(String nome, int idade, boolean vivoOuNao){
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuNao = vivoOuNao;
+    }
+
+    public Hokages(double altura) {
+        this.altura = altura;
+    }
+
+    // Você consegue criar os construtores automaticamente com o intelliJ
+    public Hokages(int idade, String nome, boolean vivoOuNao, String aldeia, int missoes, double saldoBancario, double altura) {
+        this.idade = idade;
+        this.nome = nome;
+        this.vivoOuNao = vivoOuNao;
+        this.aldeia = aldeia;
+        this.missoes = missoes;
+        this.saldoBancario = saldoBancario;
+        this.altura = altura;
+    }
 }
