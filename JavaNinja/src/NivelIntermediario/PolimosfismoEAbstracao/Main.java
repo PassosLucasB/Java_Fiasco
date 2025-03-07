77c906170b3b2922995b7faa83e36f07e3b343b6
PassosLucasB <PassosL@outlook.com>
2025-03-06 23:20:11 -0300

    "Revisao Polimorfismo e Abstracao"

---
+++ JavaNinja/src/NivelIntermediario/PolimosfismoEAbstracao/Main.java
@@ -0,0 +1,16 @@
+package NivelIntermediario.PolimosfismoEAbstracao;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Obj uzumaki
+        Uzumaki naruto = new Uzumaki();
+        naruto.nome = "Naruto Uzumaki";
+        naruto.habilidadeEspecial();
+
+        // Obj uchiha
+        Uchiha sasuke = new Uchiha();
+        sasuke.nome = "Sasuke Uchiha";
+        sasuke.habilidadeEspecial();
+    }
+}
---
+++ JavaNinja/src/NivelIntermediario/PolimosfismoEAbstracao/Uchiha.java
@@ -0,0 +1,10 @@
+package NivelIntermediario.PolimosfismoEAbstracao;
+
+public class Uchiha extends Ninja{
+
+    //Sobreescrever o método da classe Ninja
+    @Override
+    public void habilidadeEspecial(){
+        System.out.println("Meu nome é " + nome + " e esse é meu ataque Uchiha");
+    }
+}
