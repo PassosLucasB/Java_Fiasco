ad19145afdfd784489e382839829cc2f96c882d4
PassosLucasB <PassosL@outlook.com>
2025-03-10 01:15:26 -0300

    "Inicio Heranca Multipla"

---
+++ JavaNinja/src/NivelIntermediario/HerancaMultipla/Main.java
@@ -0,0 +1,14 @@
+package NivelIntermediario.HerancaMultipla;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Obj uchiha
+        Uchiha sasuke = new Uchiha();
+        sasuke.nome = "Sasuke Uchiha";
+        sasuke.aldeia = "Aldeia da Folha";
+        sasuke.idade = 18;
+        sasuke.sharinganAtivado();
+
+    }
+}
---
+++ JavaNinja/src/NivelIntermediario/HerancaMultipla/Uchiha.java
@@ -0,0 +1,8 @@
+package NivelIntermediario.HerancaMultipla;
+
+public class Uchiha extends Ninja {
+
+    public void sharinganAtivado(){
+        System.out.println(nome + ": Ativou o Sharingan");
+    }
+}
