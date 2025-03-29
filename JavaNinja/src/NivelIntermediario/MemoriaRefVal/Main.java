8af128ac6af91a187f5d16c783abe796d77afea3
PassosLucasB <PassosL@outlook.com>
2025-03-28 23:57:17 -0300

    "Inicio Memoria RefVal"

---
+++ JavaNinja/src/NivelIntermediario/MemoriaRefVal/Main.java
@@ -0,0 +1,9 @@
+package NivelIntermediario.MemoriaRefVal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("");
+        Uzumaki naruto - new Uzumaki();
+    }
+}
