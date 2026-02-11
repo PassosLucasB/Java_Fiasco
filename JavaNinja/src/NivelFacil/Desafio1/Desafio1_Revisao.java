cf4ec7df13ab5fb36d43a48db1203e693f98de86
PassosLucasB <PassosL@outlook.com>
2026-02-10 23:18:27 -0300

    "Revisao Desafio 1"

---
+++ JavaNinja/src/NivelFacil/Desafio1/Desafio1_Revisao.java
@@ -0,0 +1,11 @@
+package NivelFacil.Desafio1;
+
+public class Desafio1_Revisao {
+
+/*  Crie um programa que represente três ninjas da Vila da Folha e suas respectivas missões.
+
+    - Cada ninja tem um NOME, uma IDADE;
+    - Uma MISSÃO atribuída e ele, com o NOME DA MISSÃO, NÍVEL DE DIFICULTADE e STATUS DE CONCLUSÃO.
+*/
+
+}
