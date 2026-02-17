5b62706380dcbd2f8ffbf1639693e3510cb14559
PassosLucasB <PassosL@outlook.com>
2026-02-17 03:17:44 -0300

    "Revisao Switch e Laços"

--- JavaNinja/src/NivelFacil/Condicoes/LacosDeRepeticao.java
+++ JavaNinja/src/NivelFacil/Condicoes/LacosDeRepeticao.java
@@ -4,23 +4,30 @@ public class LacosDeRepeticao {
     public static void main(String[] args) {
 
         /*
-        *   Laços de Repetição: Vão repetir infinitamente ou até você atingir o paremetro desejado
+        *   Laços de Repetição: Vão repetir infinitamente ou até você atingir o parâmetro desejado
         *   WHILE = FOR
         *
          */
 
         //  While
-        // while (condição) {Tudo aqui vai acontecer}
+        // while (condição) {
+        //      Tudo aqui vai acontecer}
+
 
         int numeroDeClones = 0;
         int numeroMaximoDeClones = 40;
 
-        /*while (numeroDeClones <= numeroMaximoDeClones) {
+        /* while (numeroDeClones <= numeroMaximoDeClones) {
             numeroDeClones++;
             System.out.println("O Naruto fez um clone das sombras " + numeroDeClones);
         }
         */
+
+
+
         //  FOR
+
+        //atalho fori
 
         for (int i = 0; i <= numeroMaximoDeClones; i++) {
             System.out.println("O Naruto esta se clonando e ja se clonou " + i);
